820b1d7fa50ddf4c11a6d1a5ef3cefb6de6c168a
Eleanor Demis <eleanordemis@gmail.com>
2016-03-21 11:43:44 -0700

    adds rock paper permutation

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # toy-problems
+collection of coding challenges
+
+1. Rock Paper Permutation
+Given a number of rounds n, return all the possible rock-paper-scissors play possibilities for that number of rounds.
+ 
---
+++ rockPaperPermutation.js
@@ -0,0 +1,22 @@
+function rockPaperPermutation (roundCount) {
+  var letters = ["r", "p", "s"];
+  var result = [];
+
+  if (roundCount === 0) {
+    return result;
+  }
+
+  if (roundCount === 1) {
+    return letters;
+  }
+  else {
+    var prevCall = rockPaperPermutation(roundCount - 1);
+    for (var i = 0; i < letters.length; i++) {
+      var currentEl = letters[i];
+      for (var j = 0; j < prevCall.length; j++) {
+        result.push(currentEl + prevCall[j]);
+      }
+    }
+  }
+  return result;
+}
